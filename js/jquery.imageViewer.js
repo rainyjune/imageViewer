5f552e68d8144687c876a4a1f6e0aa3353e2d857
rainyjune <rainyjune@live.cn>
2014-09-28 09:48:00 +0800

    Update jquery.imageViewer.js

--- js/jquery.imageViewer.js
+++ js/jquery.imageViewer.js
@@ -2,10 +2,12 @@
   $.fn.imageViewer = function(){
     var elements = $(this);
     var scrollTop = 0;
+    var imgContainerName = "imageViewerContainer";
     
     function init() {
+      buildImageViewerContainer();
       elements.on("click", smallImgClicked);
-      $("#imageViewerContainer").on("click", largeImgClicked);
+      $("#" + imgContainerName).on("click", largeImgClicked);
       
     }
     
@@ -37,13 +39,39 @@
         $("#loadingBar").hide();
         imgElement.attr("src",largeimgUrl);
       };
-      setTimeout(function(){
+      //setTimeout(function(){
         img.src = largeimgUrl;  
-      }, 3000);
+      //}, 3000);
       
     }
     
+    /**
+     *
+     *<div id="imageViewerContainer">
+        <div class="contentContainer">
+          <img id="largeImg" src="" alt="pic1" />
+          <p id="loadingBar" style="">
+            <img src="images/loading-animation-8.gif" alt="">
+          </p>
+        </div>
+        
+    </div>
+     **/
+    
+    function buildImageViewerContainer() {
+      if($("#" + imgContainerName).length == 0){
+        var viewerContainer = $('<div id="imageViewerContainer"></div>');
+        var contentContainer = $('<div class="contentContainer">');
+        var largeImg = $('<img id="largeImg" src="" alt="pic1" />');
+        var loadingBar = $('<p id="loadingBar" style=""><img src="images/loading-animation-8.gif" alt=""></p>');
+        contentContainer.append(largeImg);
+        contentContainer.append(loadingBar);
+        viewerContainer.append(contentContainer);
+        $("body").prepend(viewerContainer);
+      }
+    }
+    
     init();
-    return element;
+    return elements;
   };
 })(jQuery);
